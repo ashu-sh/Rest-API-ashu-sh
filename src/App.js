c6b1a2e88ef6a27cfe87f1e0d3d5dc589837fad3
Ashutosh H. Shinde <102554445+ashu-sh@users.noreply.github.com>
2023-04-20 23:07:29 +0530

    App.js Code Updated
    App.js Code Error Resoved

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const port = 5000;
-const url = `mongodb+srv://Ashutosh:<Password>@cluster0.ms7wsze.mongodb.net/Student_Data?retryWrites=true&w=majority`
-const postsRoute = require('./posts');
-const bodyParser = require('body-parser');
+const Port = 3030;
+const Http_req = require("./Request_Handlers/Http_req");
+const bodyParser = require("body-parser");
 
+// connection string
+const Url = `mongodb+srv://ashutosh_shinde:GH14nVRE4pRSITj9@cluster0.ffixaew.mongodb.net/API?retryWrites=true&w=majority`;
 
+app.use(bodyParser.json());
+app.use("/api", Http_req);
 
-app.use(bodyParser.json())
-app.use('/post',postsRoute);
+mongoose.connect(Url, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
-mongoose.connect(url,{
+// Creating Server
+app.get("/", (req, res) => {
+  res.send(`connected to server !!`);
+  res.end();
+});
 
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-
-},()=>{
-   
-    console.log(`connected to Database !!!`);
-
-})
-app.get('/',(req,res)=>{
-
-    res.send(`server stated on port ${port}`);
-    res.end();
-
-}).listen(port,()=>{
-
-    console.log(`Database is running on server ${port}`);
-})
+app.listen(Port, (req, res) => {
+  console.log(`Connected to mongoDB on Port ${Port}`);
+});
